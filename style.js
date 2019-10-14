703526088f0ba0f3c96badcdd4acf4c62f6ac871
shubham <coldboy1998@gmail.com>
2019-10-14 17:00:22 +0530

    Removing News from Theme

--- style.js
+++ style.js
@@ -19,90 +19,153 @@ var forwardIcon = browser.runtime.getURL("icons/forward.png");
 
 var headLine=''; 
 var title = document.getElementsByClassName("headermain");
-if(title.length>0)
+var doxTit = document.getElementsByTagName("title")[0].innerHTML;
+
+if(doxTit.split("News").length<2)
 {
-    headLine = title[0].innerHTML
-}
-else
-{
-    headLine = document.getElementsByTagName("title")[0].innerHTML;
-}
-
-//HTML Content
-var startInsertHTML = `
-<body class="container">
-<div id="Tools" class="split left"></div>
-<div id="Folders" class="split right">
-<p class="alwaysOnTop">
-    <p class="button_left">
-        <a href="javascript: history.go(-1);">
-            <img src="${backIcon}" height="24" width = "24"/>
-        </a>
-    </p>   
-    <p class="button_right">
-    <a href="javascript: history.go(1);">
-        <img src="${forwardIcon}" height="24" width = "24"/>
-    </a>
-</p>
-    <p id="TopicHeadLine">${headLine}</p>
-</p>`;
-
-var endInsertHTML = ` </div></body>`;
-var innerContent =``;
-//alert(activeElement.length.toString()+" "+selectedElement.length.toString()+" "+allOtherElements.length.toString()+" "+nonSelectedElements.length.toString());
-
-if(activeElement.length+selectedElement.length!==0)
-{
-    var iHTML = document.head.innerHTML;
-    var oHTML = iHTML.split('<link rel="stylesheet" type="text/css" href="http://photon.bits-goa.ac.in/lms/theme/styles.php?theme=bits_theme&amp;rev=1557730337&amp;type=all">').join();
-    document.head.innerHTML = oHTML;
-
-    //<link rel="stylesheet" href="styles.css">
-    var loc = browser.runtime.getURL("style.css");
-    var styl = document.createElement("link");
-    styl.setAttribute("rel","stylesheet");
-    styl.setAttribute("href",loc);
-    styl.setAttribute("id","SchwiftyCold");
-    head.appendChild(styl); 
-
-    loc = browser.runtime.getURL("CustomInterface/css.css");
-    styl = document.createElement("link");
-    styl.setAttribute("rel","stylesheet");
-    styl.setAttribute("href",loc);
-    styl.setAttribute("id","SchwiftyCold");
-    head.appendChild(styl); 
-
-
-
-    if(activeElement.length>0)
+    if(title.length>0)
     {
-        var temp = document.body.innerHTML;
-        var count = (temp.match(/id="section/g) || []).length;
-        if(count>1)
+        headLine = title[0].innerHTML
+    }
+    else
+    {
+        headLine = document.getElementsByTagName("title")[0].innerHTML;
+    }
+
+    //HTML Content
+    var startInsertHTML = `
+    <body class="container">
+    <div id="Tools" class="split left"></div>
+    <div id="Folders" class="split right">
+    <p class="alwaysOnTop">
+        <p class="button_left">
+            <a href="javascript: history.go(-1);">
+                <img src="${backIcon}" height="24" width = "24"/>
+            </a>
+        </p>   
+        <p class="button_right">
+        <a href="javascript: history.go(1);">
+            <img src="${forwardIcon}" height="24" width = "24"/>
+        </a>
+    </p>
+        <p id="TopicHeadLine">${headLine}</p>
+    </p>`;
+
+    var endInsertHTML = ` </div></body>`;
+    var innerContent =``;
+    //alert(activeElement.length.toString()+" "+selectedElement.length.toString()+" "+allOtherElements.length.toString()+" "+nonSelectedElements.length.toString());
+
+    if(activeElement.length+selectedElement.length!==0)
+    {
+        var iHTML = document.head.innerHTML;
+        var oHTML = iHTML.split('<link rel="stylesheet" type="text/css" href="http://photon.bits-goa.ac.in/lms/theme/styles.php?theme=bits_theme&amp;rev=1557730337&amp;type=all">').join();
+        document.head.innerHTML = oHTML;
+
+        //<link rel="stylesheet" href="styles.css">
+        var loc = browser.runtime.getURL("style.css");
+        var styl = document.createElement("link");
+        styl.setAttribute("rel","stylesheet");
+        styl.setAttribute("href",loc);
+        styl.setAttribute("id","SchwiftyCold");
+        head.appendChild(styl); 
+
+        loc = browser.runtime.getURL("CustomInterface/css.css");
+        styl = document.createElement("link");
+        styl.setAttribute("rel","stylesheet");
+        styl.setAttribute("href",loc);
+        styl.setAttribute("id","SchwiftyCold");
+        head.appendChild(styl); 
+
+
+
+        if(activeElement.length>0)
         {
-            var sections=temp.split('id="section');
-
-            for(var i=1;i<count;i++)
+            var temp = document.body.innerHTML;
+            var count = (temp.match(/id="section/g) || []).length;
+            if(count>1)
             {
+                var sections=temp.split('id="section');
 
-                var section0 = sections[i];
-                //find title
-            /* var h3 = section0.split("<h3>");
-                var title="";
-                if(h3.length>0)title = h3[1].split("</h3>")[0];
-                alert(title);*/
-
-                //find content
-                var a = section0.split("<a");
-                var href='';
-                if(a.length>1)
+                for(var i=1;i<count;i++)
                 {
-                    for(var j=1;j<a.length;j++)
+
+                    var section0 = sections[i];
+                    //find title
+                /* var h3 = section0.split("<h3>");
+                    var title="";
+                    if(h3.length>0)title = h3[1].split("</h3>")[0];
+                    alert(title);*/
+
+                    //find content
+                    var a = section0.split("<a");
+                    var href='';
+                    if(a.length>1)
                     {
-                        href = a[j].split('href="')[1].split('"')[0];
-                        var name = a[j].split('href="')[1].split('</a>')[0].split('instancename">')[1].split("<")[0];
-                        var image = a[j].split('href="')[1].split('</a>')[0].split('<img src="')[1].split('"')[0];
-                        
+                        for(var j=1;j<a.length;j++)
+                        {
+                            href = a[j].split('href="')[1].split('"')[0];
+                            var name = a[j].split('href="')[1].split('</a>')[0].split('instancename">')[1].split("<")[0];
+                            var image = a[j].split('href="')[1].split('</a>')[0].split('<img src="')[1].split('"')[0];
+                            
+                            //Modify the image
+                            var re = /pdf/i; // i flag for case-insensitive
+                            var pos = image.search(re);
+                            if(pos>0){image = pdfIcon;}
+                            else
+                            {
+                                re=/document/i;
+                                pos = image.search(re);
+                                if(pos>0){image = docIcon}
+                                else
+                                {
+                                    image = folderIcon;
+                                }
+                            }
+
+                            //Modify the page
+                            innerContent+=
+                            `
+                            <a href="${href}" class="_contentOfFolder">
+                            <div class="my-fancy-container">
+                                <span class='icon-file-text my-icon'>
+                                    <img class="icon" src="${image}"/>
+                                </span>
+                                <span class="my-text">${name}</span>
+                            </div>
+                            </a>`;
+                        }
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            if(selectedElement.length-nonSelectedElements.length!=0)
+            {
+                //if it was a pdf window
+                var pdfWindow = document.body.innerHTML.split('<div class="resourcecontent resourcepdf">');
+                if(pdfWindow.length>1)
+                {
+                    var content = pdfWindow[1].split("</div>")[0];
+                    content = content.split('data="')[1].split('"')[0];
+                    innerContent+='<iframe src="'+content+'" style="width=100%; height=100%;" height="100%" width="100%">';
+                }
+
+                //if folder tree
+                var folderTree = document.getElementsByClassName("filemanager");
+            // alert("Folder Tree = "+folderTree.length.toString());
+                if(folderTree.length>0)
+                {
+                    var folders = folderTree[0].innerHTML;
+                    var a = folders.split('<a href="');
+                    for(var item=1;item<a.length;item++)
+                    {
+
+                        var href = a[item].split('"')[0];
+                        var name = a[item].split('<span class="fp-filename">')[1].split('</span>')[0];
+                        var image = a[item].split('src="')[1].split('"')[0];
+
                         //Modify the image
                         var re = /pdf/i; // i flag for case-insensitive
                         var pos = image.search(re);
@@ -118,7 +181,6 @@ if(activeElement.length+selectedElement.length!==0)
                             }
                         }
 
-                        //Modify the page
                         innerContent+=
                         `
                         <a href="${href}" class="_contentOfFolder">
@@ -132,110 +194,53 @@ if(activeElement.length+selectedElement.length!==0)
                     }
                 }
             }
-        }
-
-    }
-    else
-    {
-        if(selectedElement.length-nonSelectedElements.length!=0)
-        {
-            //if it was a pdf window
-            var pdfWindow = document.body.innerHTML.split('<div class="resourcecontent resourcepdf">');
-            if(pdfWindow.length>1)
+            else
             {
-                var content = pdfWindow[1].split("</div>")[0];
-                content = content.split('data="')[1].split('"')[0];
-                innerContent+='<iframe src="'+content+'" style="width=100%; height=100%;" height="100%" width="100%">';
-            }
 
-            //if folder tree
-            var folderTree = document.getElementsByClassName("filemanager");
-           // alert("Folder Tree = "+folderTree.length.toString());
-            if(folderTree.length>0)
-            {
-                var folders = folderTree[0].innerHTML;
-                var a = folders.split('<a href="');
-                for(var item=1;item<a.length;item++)
+                allOtherElements = document.getElementsByClassName("type_course depth_3 collapsed contains_branch");
+                //Home page when no tabs are open
+                for(var i=0;i<allOtherElements.length;i++)
                 {
+                    var href = allOtherElements[i].innerHTML.split('href="')[1].split('"')[0];
+                    var name = allOtherElements[i].innerHTML.split('href="')[1].split('</a>')[0].split('>')[1];
 
-                    var href = a[item].split('"')[0];
-                    var name = a[item].split('<span class="fp-filename">')[1].split('</span>')[0];
-                    var image = a[item].split('src="')[1].split('"')[0];
+                    var id = "SchwiftyMod_Root_Folder_"+i;
 
-                    //Modify the image
-                    var re = /pdf/i; // i flag for case-insensitive
-                    var pos = image.search(re);
-                    if(pos>0){image = pdfIcon;}
+                    if(!isHidden(id))
+                    {
+                        name = getChangedName(id,name);
+                        
+                        innerContent+=
+                        `
+                        <a id=${id} href="${href}" class="_contentOfFolder">
+                        <div class="my-fancy-container">
+                            <span class='icon-file-text my-icon'>
+                                <img class="icon" src="${folderIcon}"/>
+                            </span>
+                            <span class="my-text">${name}</span>
+                        </div>
+                        </a>`;
+                    }
                     else
                     {
-                        re=/document/i;
-                        pos = image.search(re);
-                        if(pos>0){image = docIcon}
-                        else
-                        {
-                            image = folderIcon;
-                        }
+                        innerContent+=
+                        `
+                        <a id=${id} href="${href}" class="inv">
+                        <div class="my-fancy-container">
+                            <span class='icon-file-text my-icon'>
+                                <img class="icon" src="${folderIcon}"/>
+                            </span>
+                            <span class="my-text">${name}</span>
+                        </div>
+                        </a>`;
                     }
-
-                    innerContent+=
-                    `
-                    <a href="${href}" class="_contentOfFolder">
-                    <div class="my-fancy-container">
-                        <span class='icon-file-text my-icon'>
-                            <img class="icon" src="${image}"/>
-                        </span>
-                        <span class="my-text">${name}</span>
-                    </div>
-                    </a>`;
                 }
             }
         }
-        else
-        {
-
-            allOtherElements = document.getElementsByClassName("type_course depth_3 collapsed contains_branch");
-            //Home page when no tabs are open
-            for(var i=0;i<allOtherElements.length;i++)
-            {
-                var href = allOtherElements[i].innerHTML.split('href="')[1].split('"')[0];
-                var name = allOtherElements[i].innerHTML.split('href="')[1].split('</a>')[0].split('>')[1];
-
-                var id = "SchwiftyMod_Root_Folder_"+i;
-
-                if(!isHidden(id))
-                {
-                    name = getChangedName(id,name);
-                    
-                    innerContent+=
-                    `
-                    <a id=${id} href="${href}" class="_contentOfFolder">
-                    <div class="my-fancy-container">
-                        <span class='icon-file-text my-icon'>
-                            <img class="icon" src="${folderIcon}"/>
-                        </span>
-                        <span class="my-text">${name}</span>
-                    </div>
-                    </a>`;
-                }
-                else
-                {
-                    innerContent+=
-                    `
-                    <a id=${id} href="${href}" class="inv">
-                    <div class="my-fancy-container">
-                        <span class='icon-file-text my-icon'>
-                            <img class="icon" src="${folderIcon}"/>
-                        </span>
-                        <span class="my-text">${name}</span>
-                    </div>
-                    </a>`;
-                }
-            }
-        }
+        var fullHtml = startInsertHTML+innerContent+endInsertHTML;
+        var _body = document.getElementsByTagName("body")[0];
+        _body.innerHTML=fullHtml;
     }
-    var fullHtml = startInsertHTML+innerContent+endInsertHTML;
-    var _body = document.getElementsByTagName("body")[0];
-    _body.innerHTML=fullHtml;
 }
 
 //Functions
@@ -248,7 +253,7 @@ function isHidden(id) {
         return true;
     }
     return false;
-  }
+}
 
 function getChangedName(id,name)
 {
@@ -262,12 +267,12 @@ function getChangedName(id,name)
 }
 
 
-  function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
+}
 
 function getCookie(cname) {
     var name = cname + "=";
